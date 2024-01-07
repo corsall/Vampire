da7f76b4107f10d2507793c9abda54f79799cfa0
MrOwlexe <owlexet@gmail.com>
2024-01-07 18:04:57 +0200

    Camera edited

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    Vector3 movementVector;
+
+    [SerializeField] float speed = 2f;
+    private void MovemnentSets()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movementVector = new Vector3();
+    }
+
+    void Update()
+    {
+        movementVector.x = Input.GetAxisRaw("Horizontal");
+        movementVector.y = Input.GetAxisRaw("Vertical");
+
+        movementVector *= speed;
+
+        rb.velocity = movementVector;  
+    }
+}
