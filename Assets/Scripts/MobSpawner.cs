86b35f4999727382ad05bf00a97b4111274a827c
corsall <gang121191@gmail.com>
2024-02-13 23:34:00 +0300

    added new features to mob spawner
    gut

--- Assets/Scripts/MobSpawner.cs
+++ Assets/Scripts/MobSpawner.cs
@@ -1,23 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MobSpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
-    [SerializeField] public int numberOfEnemiesToSpawn = 5;
+    [SerializeField] public int waveSize = 5;
 
     private float spawnTimer = 0f;
     [SerializeField] private float spawnInterval = 1f;
+    [SerializeField] private float rotationAngle = 15f;
     private int countObjectsSpawned = 0;
     [SerializeField] private int maxObjectsToSpawn = 30;
     [SerializeField] private float spawnRadius = 4f;
+
+    private Vector3 rotationVector;
 
     //private CrowdControl crowdManager;
 
     private void Start()
     {
         //crowdManager = this.GetComponent<CrowdControl>();
+        rotationVector = new Vector3(0, 1, 0) * spawnRadius;
     }
     private void Update()
     {
@@ -27,18 +32,27 @@ public class MobSpawner : MonoBehaviour
         {
             spawnTimer = 0f;
 
-            SpawnEnemies();
+            SpawnClockwise();
         }
     }
 
-    void SpawnEnemies()
+    void SpawnClockwise()
     {
-        for (int i = 0; i < numberOfEnemiesToSpawn; i++)
-        {
-            GameObject enemy = Instantiate(enemyPrefab, new Vector3(transform.position.x + i * spawnRadius, transform.position.y + i * spawnRadius, transform.position.z + i * spawnRadius), Quaternion.identity, this.transform);
+        rotationVector = Quaternion.Euler(0, 0, rotationAngle) * rotationVector;
 
-            enemy.name = "enemy" + (i + 1);
-            countObjectsSpawned++;
-        }
+        Vector3 spawnPosition = this.transform.position + rotationVector;
+
+        //draw spawn position
+        Debug.DrawLine(this.transform.position, spawnPosition, Color.red);
+
+        SpawnOneEnemy(spawnPosition);
+    }
+
+    void SpawnOneEnemy(Vector3 position)
+    {
+        GameObject enemy = Instantiate(enemyPrefab, position, Quaternion.identity, this.transform);
+        enemy.name = "enemy" + (countObjectsSpawned + 1);
+
+        countObjectsSpawned++;
     }
 }
