6087d83984d83331ddfb157f693d38618fbc4b73
corsall <gang121191@gmail.com>
2024-02-15 00:00:40 +0300

    fixed movement with profiler

--- Assets/Scripts/MoveTowards.cs
+++ Assets/Scripts/MoveTowards.cs
@@ -7,15 +7,14 @@ public class MoveTowards : MonoBehaviour
 {
     private GameObject player;
     private Rigidbody2D rb;
-    [SerializeField] private float force = 20.0f;
+    [SerializeField] private float force = 5.0f;
     [SerializeField] private float maxSpeed = 5.0f;
+    [SerializeField] private float drag = 5.0f;
 
     void Start()
     {   
         player = GameObject.FindWithTag("Player");
         rb = this.GetComponent<Rigidbody2D>();
-
-        this.GetComponent<Rigidbody2D>().freezeRotation = true;
 
         ConfigureRigidBody();
     }
@@ -23,46 +22,39 @@ public class MoveTowards : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 directionToPlayer = (player.transform.position - this.transform.position).normalized;
-        Vector2 velocityToAchieve = directionToPlayer * maxSpeed;
 
-        //List<GameObject> nearestObjects = GetNearestObjects(this.transform.position, radius);
+        //This is too slow
+        //Vector3 directionToPlayer = (player.transform.position - this.transform.position).normalized;
+        //Vector2 velocityToAchieve = directionToPlayer * maxSpeed;
 
-        ////Apply force to move this object from other objects
-        //foreach (GameObject obj in nearestObjects)
+        //if (rb.velocity.magnitude < maxSpeed)
         //{
-        //    if (obj != this.gameObject)
-        //    {
-        //        Vector3 directionToObject = (obj.transform.position - this.transform.position).normalized;
-        //        Vector2 forceToApply = directionToObject * crowdForce;
-        //        rb.AddForce(-forceToApply);
-        //    }
+        //    rb.AddForce(directionToPlayer * force);
+        //}
+        //else
+        //{
+        //    //Friction force appears to stop the object
+        //    //Also to stop wobliness of the object toStopRotation Vector is added
+        //    Vector2 friction = rb.velocity.normalized * -force;
+        //    Vector2 toStopRotation = (rb.mass * (-rb.velocity / Time.fixedDeltaTime) + (rb.mass * (velocityToAchieve / Time.fixedDeltaTime))).normalized * force;
+        //    rb.AddForce(friction + toStopRotation);
+        //    Debug.DrawRay(this.transform.position, toStopRotation, Color.white);
         //}
 
+        //Test
+        Vector3 directionToPlayer = (player.transform.position - this.transform.position).normalized;
 
-        if (rb.velocity.magnitude < maxSpeed)
+        if(rb.velocity.magnitude < maxSpeed)
         {
-            rb.AddForce(directionToPlayer * force);
+            rb.AddForce(directionToPlayer * force, ForceMode2D.Impulse);
         }
-        else
-        {
-            //Friction force appears to stop the object
-            //Also to stop wobliness of the object toStopRotation Vector is added
-            Vector2 friction = rb.velocity.normalized * -force;
-            Vector2 toStopRotation = (rb.mass * (-rb.velocity / Time.fixedDeltaTime) + (rb.mass * (velocityToAchieve / Time.fixedDeltaTime))).normalized * force;
-            rb.AddForce(friction + toStopRotation);
-            Debug.DrawRay(this.transform.position, toStopRotation, Color.white);
-        }
-
-        //Debug.DrawRay(this.transform.position, rb.totalForce, Color.red);
-        //Debug.DrawRay(this.transform.position, rb.velocity, Color.blue);
-
-        //Debug.Log("Speed: " + this.rb.velocity.magnitude + "   Force :" + this.rb.totalForce.magnitude);
     }
 
     private void ConfigureRigidBody()
     {
         rb.gravityScale = 0;
+        rb.drag = this.drag;
+        rb.freezeRotation = true;
     }
 
     public List<GameObject> GetNearestObjects(Vector3 position, float maxRange)
